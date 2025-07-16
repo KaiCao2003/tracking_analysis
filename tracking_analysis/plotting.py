5cbb3caaa89a6a14c1731c24449ede43a3324269
Kai Cao <kaicao@kaicao.dev>
2025-07-16 12:54:18 -0500

    back

--- tracking_analysis/plotting.py
+++ tracking_analysis/plotting.py
@@ -1,18 +1,11 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
-from matplotlib.collections import LineCollection
-from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
 def plot_trajectory_2d(pos, times, time_markers, out_path):
     fig, ax = plt.subplots()
-    # Plot trajectory as a continuous colored line
-    segments = [ [pos[i], pos[i+1]] for i in range(len(pos)-1) ]
-    lc = LineCollection(segments, array=times, cmap='rainbow', linewidth=2)
-    ax.add_collection(lc)
-    # Add colorbar
-    cbar = fig.colorbar(lc, ax=ax)
+    sc = ax.scatter(pos[:,0], pos[:,1], c=times, cmap='rainbow')
+    cbar = fig.colorbar(sc, ax=ax)
     cbar.set_label('Time (s)')
-    ax.autoscale()
     # Add markers on the trajectory
     for tm in time_markers:
         if 0 <= tm < len(times):
@@ -26,18 +19,9 @@ def plot_trajectory_2d(pos, times, time_markers, out_path):
 def plot_trajectory_3d(pos, times, time_markers, out_path):
     fig = plt.figure()
     ax  = fig.add_subplot(111, projection='3d')
-    # Plot trajectory as a continuous colored 3D line
-    segments3d = [
-        [(pos[i,0], pos[i,1], pos[i,2]), (pos[i+1,0], pos[i+1,1], pos[i+1,2])]
-        for i in range(len(pos)-1)
-    ]
-    lc = Line3DCollection(segments3d, array=times, cmap='rainbow', linewidth=2)
-    ax.add_collection(lc)
-    # Add colorbar
-    cbar = fig.colorbar(lc, ax=ax, pad=0.1)
+    sc  = ax.scatter(pos[:,0], pos[:,1], pos[:,2], c=times, cmap='rainbow')
+    cbar = fig.colorbar(sc, ax=ax)
     cbar.set_label('Time (s)')
-    # Autoscale axes to data limits
-    ax.auto_scale_xyz(pos[:,0], pos[:,1], pos[:,2])
     for tm in time_markers:
         if 0 <= tm < len(times):
             ax.scatter(
